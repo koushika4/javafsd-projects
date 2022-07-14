d657a62027fc22c8d1cf29f0b04ca806e3c1ea66
koushikabalusu <koushika1204@sasi.ac.in>
2022-07-14 09:38:49 +0530

    changes

---
+++ hello world/src/stringmethods.java
@@ -0,0 +1,42 @@
+
+public class stringmethods {
+	
+	public static void main(String[] args) {
+		
+		String s = "Hello World";
+		System.out.println("Length = " + s.length());
+		
+		System.out.println("Index of W = " + s.indexOf(0);
+		
+		//print the character at position 3
+		System.out.println("Character at 3 = " + s.charAt(3));
+		System.out.println("Character at 12 = " + s.charAt(12));
+		
+		System.out.println("Command after exception");
+		
+		System.out.println("Index of l = " + s.index0f('l'));
+		System.out.println("Index of second 1 = " + s.index0f('l',3));
+				System.out.println("Index of third l = " + s.indexof('l',4));
+				
+				System.out.println("substring: " + s.substring(3));
+				System.out.println("only lowo: " + s.substring(3,8));
+				
+				String[] s2 = s.split(" ");
+				System.out.println(s2[0]);
+				System.out.println(s2[1]);
+				
+				String[] s3 = s.split("o");
+				System.out.println("no of derived words: " + s3.length);
+				System.out.println(s3[0]);
+				System.out.println(s3[1]);
+				System.out.println(s3[2]);
+				
+				System.out.println("index of space: " + s.indexof(" "));
+				
+				System.out.println("Replaced: " + s.replace("l", "j"));
+				
+				System.out.println("Uppercase: " + s.toUppercase());
+				System.out.println("Lowercase: " + s.toLowercase());
+	}
+
+}
