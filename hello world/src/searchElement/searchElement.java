f4ea2e6a737676c8622fe42458f92460b01ee388
koushikabalusu <koushika1204@sasi.ac.in>
2022-07-18 20:17:18 +0530

    changes

---
+++ hello world/src/searchElement/searchElement.java
@@ -0,0 +1,40 @@
+package searchElement;
+
+import java.util.ArrayList;
+
+
+public class searchElement {
+	
+	public static void main(String[] args) {
+		
+		
+		ArrayList<String> emailID = new ArrayList<String>();
+		
+		emailID.add("koushi.balusu@gmail.com");
+		emailID.add("praneetha.katta@gmail.com");
+		emailID.add("phani.sri@gmail.com");
+		emailID.add("mouni.bhagya@gmail.com");
+		emailID.add("teja.bodapudi@gmail.com");
+		emailID.add("shanu.pendiyala@gmail.com");
+		emailID.add("lohi.kanumuri@gmail.com");
+		
+		
+		String searchElement = "koushi.balusu@gmail.com";
+		
+		for(int i=0; i<emailID.size(); i++) {
+			
+			System.out.println(emailID.get(i));
+			
+			if(searchElement==emailID.get(i)) {
+				
+				System.out.println("\n");
+				
+				System.out.println("email ID" + searchElement + "found");
+				
+				break;
+			}
+		}
+			
+	}
+
+}
