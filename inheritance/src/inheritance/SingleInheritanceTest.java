9058ce0482a1a3df93cbdb995c923bacc17fbbe4
koushikabalusu <koushika1204@sasi.ac.in>
2022-07-19 10:48:57 +0530

    changes

---
+++ inheritance/src/inheritance/SingleInheritanceTest.java
@@ -0,0 +1,17 @@
+package inheritance;
+
+import singleinheritance.SingleInheritance;
+
+public class SingleInheritanceTest {
+
+	public static void main(String[] args) {
+		SingleInheritance si = new SingleInheritance();
+		si.m1();
+		System.out.println(si.getClass());
+		System.out.println(si.hashCode());
+		System.out.println(si.toString());
+		SingleInheritance si2 = new SingleInheritance();
+		System.out.println(si2.toString());
+	}
+	
+}
---
+++ inheritance/src/singleinheritance/SingleInheritance.java
@@ -0,0 +1,8 @@
+package singleinheritance;
+
+public class SingleInheritance {
+
+	public void m1() {
+		System.out.println("Inside Method m1()");
+	}
+}
