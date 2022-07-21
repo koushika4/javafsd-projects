d238dcf762cc0ad395d2498a16abe829a6056e5d
koushikabalusu <koushika1204@sasi.ac.in>
2022-07-21 11:34:27 +0530

    changes

---
+++ Exceptions/src/exceptionsdemo/NullPointerDemo.java
@@ -0,0 +1,12 @@
+package exceptionsdemo;
+
+public class NullPointerDemo {
+	
+	public static void main(String[] args, String hi) {
+		
+		String s = hi;
+		System.out.println("String Length: " + s.length());
+		
+	}
+
+}
---
+++ Exceptions/src/exceptionsdemo/StringParser.java
@@ -0,0 +1,13 @@
+package exceptionsdemo;
+
+public class StringParser {
+
+	public static void main(String[] args) {
+		
+		String s = "1234";
+		int a = Integer.parseInt(s);
+		System.out.println("Result: " + a);
+		
+	}
+	
+}
