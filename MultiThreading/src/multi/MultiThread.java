7c59e8ed9a8e3d604328f543740e73c5e38748b5
koushikabalusu <koushika1204@sasi.ac.in>
2022-07-22 10:55:27 +0530

    changes

--- MultiThreading/src/multi/MultiThread.java
+++ MultiThreading/src/multi/MultiThread.java
@@ -1,5 +1,25 @@
 package multi;
 
-public class MultiThread {
+public class MultiThread extends Thread {
+	
+	public static void main(String[] args) throws InterruptedException {
+		MultiThread mt = new MultiThread();
+		mt.start();
+		
+		for (int j=1; j<=200; j++) {
+			System.out.print("j: " + j + "\t");
+			Thread.sleep(500);
+		}
+	}
 
-}
+	@Override
+	public void run() {
+		for (int i=1; i<=200; i++) {
+			System.out.println("i: " + i + "\t");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
