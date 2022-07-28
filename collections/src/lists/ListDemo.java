477f388fc393e53a0ae53a430dc7b70e711bf896
koushikabalusu <koushika1204@sasi.ac.in>
2022-07-28 12:31:56 +0530

    changes

---
+++ collections/src/lists/ListDemo.java
@@ -0,0 +1,25 @@
+package lists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ListDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList list = new ArrayList();
+		list.add(1);
+		list.add(2);
+		list.add(30);
+		list.add("Hello");
+		list.add(30.45);
+		System.out.println(list);
+		
+		
+		ArrayList<Integer> my ArrayList integerlist = new ArrayList<>();
+		myintegerslist.add(10);
+		myintegerslist.add(new Integer(20));
+		myintegerslist.add(30);
+		System.out.println(myintegerslist);
+	}
+}
